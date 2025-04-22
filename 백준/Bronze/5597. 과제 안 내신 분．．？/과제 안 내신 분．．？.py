8176389c298f0aea13bf785762bb8bcadb4c1bcd
dlel02 <160707046+dlel02@users.noreply.github.com>
2025-04-22 16:48:51 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -2,7 +2,7 @@ import sys
 student_number = []
 
 for _ in sys.stdin:
-    student_number.append(int(_.strip()))
+    student_number.append(int(_))
 for i in range(1, 31):
     if i not in student_number:
         print(i)       
